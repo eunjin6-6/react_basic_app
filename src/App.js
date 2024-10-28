5131ce6609c4bf8531a18ba78c4d2c9fccfa5830
eunjin6-6 <dmswls616@gmail.com>
2024-10-28 16:16:11 +0900

    menu 추가

--- src/App.js
+++ src/App.js
@@ -21,7 +21,8 @@ class App extends Component {
       menus:[
         {id:1, title:'HTML', desc:'Hypertext Markup Language'},
         {id:2, title:'CSS', desc:'CSS for design'},
-        {id:3, title:'Javascript', desc:'Javascript for interaction'}
+        {id:3, title:'Javascript', desc:'Javascript for interaction'},
+        {id:4, title:'React', desc:'Single page Application'}
       ]
     };
   }
